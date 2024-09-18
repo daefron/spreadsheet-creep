39d39af3f98b6f89fb1b0180d8998c3507495b83
Daefron <thomas_evans@outlook.com>
2024-09-18 18:52:01 +1000

    add entity creator

--- src/Game Logic/Engine.jsx
+++ src/Game Logic/Engine.jsx
@@ -1,52 +1,76 @@
 export default function Engine() {
-
   //hobject that holds values of entities
   const entityList = {
     goblin: {
       name: "Goblin",
       enemy: true,
-      position: "B1",
       newPosition: undefined,
-      lvl1: {
-        hp: 3,
-        dmg: 1,
-        range: 1,
-        rate: 2,
-        speed: 1,
-      },
-      lvl2: {
-        hp: 5,
-        dmg: 2,
-        range: 1,
-        rate: 2,
-        speed: 1,
+      levels: {
+        lvl1: {
+          name: "Level 1",
+          hp: 3,
+          dmg: 1,
+          range: 1,
+          rate: 2,
+          speed: 1,
+        },
+        lvl2: {
+          name: "Level 2",
+          hp: 5,
+          dmg: 2,
+          range: 1,
+          rate: 2,
+          speed: 1,
+        },
       },
     },
     arrow: {
       name: "Arrow",
       enemy: false,
-      position: "E1",
       newPosition: undefined,
-      lvl1: {
-        hp: 5,
-        dmg: 2,
-        range: 3,
-        rate: 2,
-        speed: 0,
-      },
-      lvl2: {
-        hp: 8,
-        dmg: 4,
-        range: 4,
-        rate: 2,
-        speed: 0,
+      levels: {
+        lvl1: {
+          name: "Level 1",
+          hp: 5,
+          dmg: 2,
+          range: 3,
+          rate: 2,
+          speed: 0,
+        },
+        lvl2: {
+          name: "Level 2",
+          hp: 8,
+          dmg: 4,
+          range: 4,
+          rate: 2,
+          speed: 0,
+        },
       },
     },
   };
 
   //array that holds active entities
-  let activeEntities = [entityList.goblin, entityList.arrow];
+  let activeEntities = [];
 
+  //function that creates new entities
+  function Entity(type, level, position, activeEntities) {
+    this.name = type.name;
+    this.level = level.name;
+    this.hp = level.hp;
+    this.dmg = level.dmg;
+    this.range = level.range;
+    this.rate = level.rate;
+    this.speed = level.speed;
+    this.enemy = type.enemy;
+    this.position = position;
+    activeEntities.push(this);
+    gameboardUpdater(activeEntities, gameboard);
+  }
+
+  //default positions for goblins to spawn (far side of map)
+  const goblinSpawn = "J1";
+
+  
   //function to make the gameboard grid
   let gameboard = new Map();
   function gameboardMaker(gameboard) {
@@ -57,7 +81,7 @@ export default function Engine() {
     return gameboard;
   }
   gameboardMaker(gameboard);
-
+  
   //function to update the gameboard entities
   function gameboardUpdater(activeEntities, gameboard) {
     activeEntities.forEach((entity) => {
@@ -70,13 +94,11 @@ export default function Engine() {
     console.log(gameboard);
     return gameboard;
   }
-  gameboardUpdater(activeEntities, gameboard);
-
+  
   //function to check if the space to the left is free, and if so, moves the enemy
   function enemyMovement(currentEntity) {
     let oldPosition = currentEntity.position;
-    let newPosition =
-      letterChecker(oldPosition) + oldPosition.charAt(1);
+    let newPosition = letterChecker(oldPosition) + oldPosition.charAt(1);
     if (gameboard.get(newPosition) == undefined) {
       gameboard.set(oldPosition, undefined);
       currentEntity.position = newPosition;
@@ -84,16 +106,16 @@ export default function Engine() {
     } else console.log("position taken");
   }
   nextTurn(activeEntities);
-
+  
   //function to initiate next turn actions
   function nextTurn(activeEntities) {
-    activeEntities.forEach(entity => {
+    activeEntities.forEach((entity) => {
       if (entity.enemy == true) {
         enemyMovement(entity);
       }
     });
   }
-
+  
   //lazy function to go back one in the alphabet
   function letterChecker(position) {
     let letter = position.charAt(0);
@@ -121,8 +143,17 @@ export default function Engine() {
     return letter;
   }
 
-  return (
-    <>
-    </>
+  //entities to test the Entity object creator
+  const testGoblin = new Entity(
+    entityList.goblin,
+    entityList.goblin.levels.lvl2, goblinSpawn,
+    activeEntities
   );
+  const testArrow = new Entity(
+    entityList.arrow,
+    entityList.arrow.levels.lvl2, "B1",
+    activeEntities
+  );
+  
+  return <></>;
 }
