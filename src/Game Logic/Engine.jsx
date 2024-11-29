c12959ac5f4bffad596f1e637b8e4e4eeb675b6f
Daefron <thomas_evans@outlook.com>
2024-11-29 15:33:01 +1100

    moved all gamemodes to gamemmodes file

--- src/Game Logic/Engine.jsx
+++ src/Game Logic/Engine.jsx
@@ -1,25 +1,20 @@
+import { terrainMaker, ghoster } from "./Engine/EngineTools.jsx";
 import {
-  entitySpawner,
-  spawnChecker,
-  terrainMaker,
-  ghoster,
-} from "./EngineTools.jsx";
+  kingTurn,
+  battleTurn,
+  blobTurn,
+  blobFightTurn,
+  blobGobTurn,
+  sandboxTurn,
+} from "./Engine/GameModes.jsx";
 export function engine(gameState) {
   let activeEntities = gameState.active.activeEntities;
   let activeProjectiles = gameState.active.activeProjectiles;
   let activeGround = gameState.active.activeGround;
   let activeFluid = gameState.active.activeFluid;
   let activeEffects = gameState.active.activeEffects;
-  let enemySpawnCount = gameState.engine.enemySpawnCount;
-  let friendlySpawnCount = gameState.engine.friendlySpawnCount;
-  let lastEnemySpawnTime = gameState.engine.lastEnemySpawnTime;
-  let timer = gameState.engine.timer;
-  let gameSpeed = gameState.settings.gameSpeed;
-  let totalSpawns = gameState.settings.totalSpawns;
   let gameMode = gameState.settings.gameMode;
-  let gameStatus = gameState.render.gameStatus;
   let newRound = gameState.engine.newRound;
-  let blobAtEnd = gameState.engine.blobAtEnd;
   let gameStatePacked = gameState;
 
   if (newRound.current) {
@@ -27,104 +22,27 @@ export function engine(gameState) {
     terrainMaker(gameStatePacked);
     newRound.current = false;
   }
+
   gamemode();
 
   function gamemode() {
     if (gameMode.current === "king") {
-      gameStatus.current = "King round in progress";
-      return kingTurn();
+      return kingTurn(gameStatePacked, nextTurn);
     }
     if (gameMode.current === "battle") {
-      gameStatus.current = "Battle round in progress";
-      return battleTurn();
+      return battleTurn(gameStatePacked, nextTurn);
     }
     if (gameMode.current === "blob") {
-      gameStatus.current = "Blob round in progress";
-      return blobTurn();
+      return blobTurn(gameStatePacked, nextTurn);
     }
     if (gameMode.current === "blob fight") {
-      gameStatus.current = "Blob fight in progress";
-      return blobFightTurn();
+      return blobFightTurn(gameStatePacked, nextTurn);
     }
     if (gameMode.current === "blob gob") {
-      gameStatus.current = "Blob vs enemies round in progress";
-      return blobGobTurn();
+      return blobGobTurn(gameStatePacked, nextTurn);
     }
     if (gameMode.current === "sandbox") {
-      gameStatus.current = "Sandbox in progress";
-      return sandboxTurn();
-    }
-
-    function kingTurn() {
-      if (!friendlySpawnCount.current) {
-        entitySpawner(["king", 1], false, gameStatePacked);
-        friendlySpawnCount.current++;
-      }
-      spawnChecker(true, gameStatePacked);
-      nextTurn();
-      if (!victoryChecker()) {
-        clearInterval(timer.current);
-      }
-    }
-
-    function battleTurn() {
-      spawnChecker(true, gameStatePacked);
-      spawnChecker(false, gameStatePacked);
-      nextTurn();
-      if (!victoryChecker()) {
-        clearInterval(timer.current);
-      }
-    }
-
-    function blobTurn() {
-      if (lastEnemySpawnTime.current < 201) {
-        lastEnemySpawnTime.current++;
-      }
-      if (lastEnemySpawnTime.current === 200) {
-        let firstBlob = entitySpawner(["blob", 1], true, gameStatePacked);
-        firstBlob.hp = firstBlob.maxHp;
-        enemySpawnCount.current++;
-      }
-      nextTurn();
-      if (!victoryChecker()) {
-        clearInterval(timer.current);
-      }
-    }
-
-    function blobFightTurn() {
-      if (!activeEntities.current.length) {
-        lastEnemySpawnTime.current++;
-      }
-      if (
-        lastEnemySpawnTime.current === 200 / gameSpeed.current &&
-        !activeEntities.current.length
-      ) {
-        let firstBlob = entitySpawner(["blob", 1], true, gameStatePacked);
-        firstBlob.hp = firstBlob.maxHp;
-        let secondBlob = entitySpawner(["blob", 1], false, gameStatePacked);
-        secondBlob.hp = secondBlob.maxHp;
-      }
-      nextTurn();
-      if (!victoryChecker()) {
-        clearInterval(timer.current);
-      }
-    }
-
-    function blobGobTurn() {
-      lastEnemySpawnTime.current++;
-      if (lastEnemySpawnTime.current === 100 / gameSpeed.current) {
-        let firstBlob = entitySpawner(["blob", 1], true, gameStatePacked);
-        firstBlob.hp = firstBlob.maxHp * 5;
-      }
-      spawnChecker(false, gameStatePacked);
-      nextTurn();
-      if (!victoryChecker()) {
-        clearInterval(timer.current);
-      }
-    }
-
-    function sandboxTurn() {
-      nextTurn();
+      return sandboxTurn(nextTurn);
     }
 
     function nextTurn() {
@@ -142,112 +60,6 @@ export function engine(gameState) {
       }
       for (const effect of activeEffects.current) {
         effect.turn;
-      }
-    }
-
-    function victoryChecker() {
-      if (gameMode.current === "king") {
-        let kingAlive = activeEntities.current.find(
-          (entity) => entity.type === "king"
-        );
-        if (!kingAlive && totalSpawns.current) {
-          gameStatus.current = "King dead, you lose";
-          return false;
-        }
-        let enemiesAlive = activeEntities.current.filter(
-          (entity) => entity.enemy
-        );
-        if (
-          enemySpawnCount.current >= totalSpawns.current &&
-          !enemiesAlive.length
-        ) {
-          gameStatus.current = "All enemies dead";
-          return false;
-        }
-        return true;
-      }
-      if (gameMode.current === "battle") {
-        let enemiesAlive = activeEntities.current.filter(
-          (entity) => entity.enemy
-        );
-        if (
-          enemySpawnCount.current >= totalSpawns.current &&
-          !enemiesAlive.length
-        ) {
-          gameStatus.current = "All enemies dead";
-          return false;
-        }
-        let friendliesAlive = activeEntities.current.filter(
-          (entity) => !entity.enemy
-        );
-        if (
-          friendlySpawnCount.current >= totalSpawns.current &&
-          !friendliesAlive.length
-        ) {
-          gameStatus.current = "All friendlies dead";
-          return false;
-        }
-        return true;
-      }
-      if (gameMode.current === "blob") {
-        if (blobAtEnd.current) {
-          gameStatus.current = "Blob at end. You lose";
-          return false;
-        }
-        let enemiesAlive = activeEntities.current.filter(
-          (entity) => entity.enemy
-        );
-        if (!enemiesAlive.length && enemySpawnCount.current) {
-          gameStatus.current = "Enemy blob dead";
-          return false;
-        }
-        return true;
-      }
-      if (gameMode.current === "blob fight") {
-        let enemiesAlive = activeEntities.current.filter(
-          (entity) => entity.enemy
-        );
-        let friendliesAlive = activeEntities.current.filter(
-          (entity) => !entity.enemy
-        );
-        if (
-          !enemiesAlive.length &&
-          friendliesAlive.length &&
-          enemySpawnCount.current
-        ) {
-          gameStatus.current = "Enemy blob dead";
-          return false;
-        }
-        if (
-          !friendliesAlive.length &&
-          enemiesAlive.length &&
-          friendlySpawnCount.current
-        ) {
-          gameStatus.current = "Friendly blob dead";
-          return false;
-        }
-        return true;
-      }
-      if (gameMode.current === "blob gob") {
-        let friendliesAlive = activeEntities.current.filter(
-          (entity) => !entity.enemy
-        );
-        if (
-          (friendlySpawnCount.current === totalSpawns.current &&
-            !friendliesAlive.length) ||
-          blobAtEnd
-        ) {
-          gameStatus.current = "Friendlies lose";
-          return false;
-        }
-        let enemiesAlive = activeEntities.current.filter(
-          (entity) => entity.enemy
-        );
-        if (!enemiesAlive.length && friendlySpawnCount.current > 1) {
-          gameStatus.current = "Enemies lose";
-          return false;
-        }
-        return true;
       }
     }
   }
---
+++ src/Game Logic/Engine/GameModes.jsx
@@ -0,0 +1,205 @@
+import { entitySpawner, spawnChecker } from "./EngineTools.jsx";
+export function kingTurn(gameState, nextTurn) {
+  let enemySpawnCount = gameState.engine.enemySpawnCount;
+  let friendlySpawnCount = gameState.engine.friendlySpawnCount;
+  let activeEntities = gameState.active.activeEntities;
+  let totalSpawns = gameState.settings.totalSpawns;
+  let gameStatus = gameState.render.gameStatus;
+  let timer = gameState.engine.timer;
+  gameStatus.current = "King round in progress";
+  if (!friendlySpawnCount.current) {
+    entitySpawner(["king", 1], false, gameState);
+    friendlySpawnCount.current++;
+  }
+  spawnChecker(true, gameState);
+  nextTurn();
+
+  if (!kingVictory()) {
+    clearInterval(timer.current);
+  }
+
+  function kingVictory() {
+    let kingAlive = activeEntities.current.find(
+      (entity) => entity.type === "king"
+    );
+    if (!kingAlive && totalSpawns.current) {
+      gameStatus.current = "King dead, you lose";
+      return false;
+    }
+    let enemiesAlive = activeEntities.current.filter((entity) => entity.enemy);
+    if (
+      enemySpawnCount.current >= totalSpawns.current &&
+      !enemiesAlive.length
+    ) {
+      gameStatus.current = "All enemies dead";
+      return false;
+    }
+    return true;
+  }
+}
+export function battleTurn(gameState, nextTurn) {
+  let gameStatus = gameState.render.gameStatus;
+  let activeEntities = gameState.active.activeEntities;
+  let enemySpawnCount = gameState.engine.enemySpawnCount;
+  let totalSpawns = gameState.settings.totalSpawns;
+  let timer = gameState.engine.timer;
+  let friendlySpawnCount = gameState.engine.friendlySpawnCount;
+  gameStatus.current = "Battle round in progress";
+  spawnChecker(true, gameState);
+  spawnChecker(false, gameState);
+  nextTurn();
+  if (!battleVictory()) {
+    clearInterval(timer.current);
+  }
+  function battleVictory() {
+    let enemiesAlive = activeEntities.current.filter((entity) => entity.enemy);
+    if (
+      enemySpawnCount.current >= totalSpawns.current &&
+      !enemiesAlive.length
+    ) {
+      gameStatus.current = "All enemies dead";
+      return false;
+    }
+    let friendliesAlive = activeEntities.current.filter(
+      (entity) => !entity.enemy
+    );
+    if (
+      friendlySpawnCount.current >= totalSpawns.current &&
+      !friendliesAlive.length
+    ) {
+      gameStatus.current = "All friendlies dead";
+      return false;
+    }
+    return true;
+  }
+}
+
+export function blobTurn(gameState, nextTurn) {
+  let gameStatus = gameState.render.gameStatus;
+  let activeEntities = gameState.active.activeEntities;
+  let enemySpawnCount = gameState.engine.enemySpawnCount;
+  let blobAtEnd = gameState.engine.blobAtEnd;
+  let timer = gameState.engine.timer;
+  let lastEnemySpawnTime = gameState.engine.lastEnemySpawnTime;
+  gameStatus.current = "Blob round in progress";
+  if (lastEnemySpawnTime.current < 201) {
+    lastEnemySpawnTime.current++;
+  }
+  if (lastEnemySpawnTime.current === 200) {
+    let firstBlob = entitySpawner(["blob", 1], true, gameState);
+    firstBlob.hp = firstBlob.maxHp;
+    enemySpawnCount.current++;
+  }
+  nextTurn();
+  if (!blobVictory()) {
+    clearInterval(timer.current);
+  }
+  function blobVictory() {
+    if (blobAtEnd.current) {
+      gameStatus.current = "Blob at end. You lose";
+      return false;
+    }
+    let enemiesAlive = activeEntities.current.filter((entity) => entity.enemy);
+    if (!enemiesAlive.length && enemySpawnCount.current) {
+      gameStatus.current = "Enemy blob dead";
+      return false;
+    }
+    return true;
+  }
+}
+
+export function blobFightTurn(gameState, nextTurn) {
+  let gameStatus = gameState.render.gameStatus;
+  let activeEntities = gameState.active.activeEntities;
+  let enemySpawnCount = gameState.engine.enemySpawnCount;
+  let friendlySpawnCount = gameState.engine.friendlySpawnCount;
+  let timer = gameState.engine.timer;
+  let lastEnemySpawnTime = gameState.engine.lastEnemySpawnTime;
+  let gameSpeed = gameState.settings.gameSpeed;
+  gameStatus.current = "Blob fight in progress";
+  if (!activeEntities.current.length) {
+    lastEnemySpawnTime.current++;
+  }
+  if (
+    lastEnemySpawnTime.current === 200 / gameSpeed.current &&
+    !activeEntities.current.length
+  ) {
+    let firstBlob = entitySpawner(["blob", 1], true, gameState);
+    firstBlob.hp = firstBlob.maxHp;
+    let secondBlob = entitySpawner(["blob", 1], false, gameState);
+    secondBlob.hp = secondBlob.maxHp;
+  }
+  nextTurn();
+  if (!blobFightVictory()) {
+    clearInterval(timer.current);
+  }
+  function blobFightVictory() {
+    let enemiesAlive = activeEntities.current.filter((entity) => entity.enemy);
+    let friendliesAlive = activeEntities.current.filter(
+      (entity) => !entity.enemy
+    );
+    if (
+      !enemiesAlive.length &&
+      friendliesAlive.length &&
+      enemySpawnCount.current
+    ) {
+      gameStatus.current = "Enemy blob dead";
+      return false;
+    }
+    if (
+      !friendliesAlive.length &&
+      enemiesAlive.length &&
+      friendlySpawnCount.current
+    ) {
+      gameStatus.current = "Friendly blob dead";
+      return false;
+    }
+    return true;
+  }
+}
+
+export function blobGobTurn(gameState, nextTurn) {
+  let gameStatus = gameState.render.gameStatus;
+  let activeEntities = gameState.active.activeEntities;
+  let friendlySpawnCount = gameState.engine.friendlySpawnCount;
+  let gameSpeed = gameState.settings.gameSpeed;
+  let lastEnemySpawnTime = gameState.engine.lastEnemySpawnTime;
+  let totalSpawns = gameState.settings.totalSpawns;
+  let timer = gameState.engine.timer;
+  let blobAtEnd = gameState.engine.blobAtEnd;
+  gameStatus.current = "Blob vs enemies round in progress";
+  lastEnemySpawnTime.current++;
+  if (lastEnemySpawnTime.current === 100 / gameSpeed.current) {
+    let firstBlob = entitySpawner(["blob", 1], true, gameState);
+    firstBlob.hp = firstBlob.maxHp * 5;
+  }
+  spawnChecker(false, gameState);
+  nextTurn();
+  if (!blobGobVictory()) {
+    clearInterval(timer.current);
+  }
+  function blobGobVictory() {
+    let friendliesAlive = activeEntities.current.filter(
+      (entity) => !entity.enemy
+    );
+    if (
+      (friendlySpawnCount.current === totalSpawns.current &&
+        !friendliesAlive.length) ||
+      blobAtEnd.current
+    ) {
+      gameStatus.current = "Friendlies lose";
+      return false;
+    }
+    let enemiesAlive = activeEntities.current.filter((entity) => entity.enemy);
+    if (!enemiesAlive.length && friendlySpawnCount.current > 1) {
+      gameStatus.current = "Enemies lose";
+      return false;
+    }
+    return true;
+  }
+}
+
+export function sandboxTurn(nextTurn) {
+  gameStatus.current = "Sandbox in progress";
+  nextTurn();
+}
